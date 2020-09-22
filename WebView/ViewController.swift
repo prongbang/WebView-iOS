b58102c29285e3bd3a3939269832070dc1910f0c
anonymous <anonymous>
2020-09-22 14:49:30 +0700

    init

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import SafariServices
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
+    
+    private let webUrl = "https://fix-asset-app.herokuapp.com/#/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.checkPermissions()
     }
+    
+    private func setupCaptureSession() {
+        DispatchQueue.main.async {
+            self.loadSafari()
+        }
+    }
+    
+    private func checkPermissions() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized: // The user has previously granted access to the camera.
+                self.setupCaptureSession()
+            
+            case .notDetermined: // The user has not yet been asked for camera access.
+                AVCaptureDevice.requestAccess(for: .video) { granted in
+                    if granted {
+                        self.setupCaptureSession()
+                    }
+                }
+            
+            case .denied: // The user has previously denied access.
+                return
 
-
+            case .restricted: // The user can't grant access due to restrictions.
+                return
+        }
+    }
+    
+    private func loadSafari() {
+        if let url = URL(string: self.webUrl) {
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+            
+            let vc = SFSafariViewController(url: url, configuration: config)
+            vc.delegate = self
+            present(vc, animated: true)
+        }
+    }
+    
 }
 
